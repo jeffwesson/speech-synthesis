a701a397417bf6b021444d1eec4a0a1e81ac1754
jeffwesson <jeff@jeffwesson.com>
2014-08-31 02:46:17 -0700

    adding geolocation

--- components/main/main.controller.js
+++ components/main/main.controller.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function MainCtrl() {
+  function MainCtrl($window, $timeout, $log) {
     this.mainThings = [
       {
         name: 'one',
@@ -17,11 +17,22 @@
       }
     ];
 
+    this.position = {};
+
+    this.getLocation = function() {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $log.log(position.coords.latitude);
+        $log.log(position.coords.longitude);
+      });
+    };
+
     this.utterance = '';
 
     this.speakUtterance = function() {
       var saying = new SpeechSynthesisUtterance(this.utterance);
-      window.speechSynthesis.speak(saying);
+      $window.speechSynthesis.speak(saying);
+      $timeout($window.speechSynthesis.speak(this.position.latitude.toString()), 2000);
+      $timeout($window.speechSynthesis.speak(this.position.longitude.toString()), 2000);
     };
   }
 
